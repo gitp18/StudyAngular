1d5810bed5b6d11cf8cf773664cf94d11746f3b8
ipsita <developer.ipsita@gmail.com>
2020-12-27 17:07:47 +0530

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomDirectiveComponent } from './custom-directive/custom-directive.component';
+import { ClassDirective } from './class.directive';
+import { UserComponent } from './user/user.component';
+import { SignupComponent } from './signup/signup.component';
 
-import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryPipe
+    CustomDirectiveComponent,
+    ClassDirective,
+    UserComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+//import { Employee } from '../Models/user.model';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  /*employee: Employee = {
+    name: null,
+    email: null,
+    gender: null,
+    department: null,
+    isActive: null
+  }*/
+  departments = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'}
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
