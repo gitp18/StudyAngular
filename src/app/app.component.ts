b1264eb03e9167a09d889b608176fef7d6e42fb5
ipsita <developer.ipsita@gmail.com>
2021-01-01 16:17:46 +0530

    Angular download copy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  course = {
-    title: "StudyAngular",
-    subject: "FRONTEND",
-    students: 5000,
-    ratings: '4.304',
-    views: '4.9745',
-    price: '190.95',
-    releaseDate: new Date(2016,3,1) 
-  }
-
-  articleDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SummaryPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
